3ab7a5f0072584ca0140517c711b3e732d5256a9
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2019-02-11 12:09:58 -0800

    Added basic component structure.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 // import sortArrBy from './utility/sortArrBy.js';
-
+import Header from './Header';
+import Filter from './Filter';
+import Sort from './Sort';
+import Board from './Board';
+import Footer from './Footer';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  componentDidMount() {
+    console.log('mounted, fetching data');
+  }
+
   render() {
-    return <div className="App">{/*  */}</div>;
+    return (
+      <div className="App">
+        <Header />
+        <Filter />
+        <Sort />
+        <Board />
+        <Footer />
+      </div>
+    );
   }
 }
 
---
+++ src/Sort.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Sort extends Component {
+  render() {
+    return 'Sort';
+  }
+}
